1f3b6b3dd8fd93fc89d7e8998dd71c9d8d6a84f6
afsal.vh <afsalafsu821@gmail.com>
2025-05-12 21:04:57 +0530

    Added Suspense

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { BarLoader } from "react-spinners";
 
 const MainLayout = ({ children }) => {
-  return <div className="mt-20">{children}</div>;
+  return (
+    <div className="mt-20">
+      <Suspense fallback={<BarLoader width={"100%"} color="#36d7b7" />}>
+        {children}
+      </Suspense>
+    </div>
+  );
 };
 
 export default MainLayout;
